f64c11b6bccff5457261902cd035392f7d233ae6
itsharry1189 <hareesh@karnanisoft.com>
2017-02-26 11:38:51 +0530

    add,sub,mul,div of two numbers with java 8

--- addnumbers/src/main/java/hareesh/com/addnumbers/MainActivity.java
+++ addnumbers/src/main/java/hareesh/com/addnumbers/MainActivity.java
@@ -17,8 +17,15 @@ public class MainActivity extends AppCompatActivity {
         EditText num2=(EditText) findViewById(R.id.num2);
         EditText result=(EditText) findViewById(R.id.result);
         Button okbtn=(Button) findViewById(R.id.btnok);
+        Button btnsub=(Button) findViewById(R.id.btnsub);
+        Button btnmul=(Button) findViewById(R.id.btnmul);
+        Button btndiv=(Button) findViewById(R.id.btndiv);
 
         okbtn.setOnClickListener(this::addnumber);
+        btnsub.setOnClickListener(this::subnum);
+        btnmul.setOnClickListener(this::mulnum);
+        btndiv.setOnClickListener(this::divnum);
+
 
 
 
@@ -31,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         int n1,n2,sum;
         EditText num1=(EditText) findViewById(R.id.num1);
         EditText num2=(EditText) findViewById(R.id.num2);
+
         EditText result=(EditText) findViewById(R.id.result);
 
         n1=Integer.parseInt(num1.getText().toString());
@@ -38,11 +46,50 @@ public class MainActivity extends AppCompatActivity {
         sum = n1 + n2;
         result.setText(Integer.toString(sum));
 
+    }
 
+    public void subnum(View view)
+    {
+        int n1,n2,sub;
+        EditText num1=(EditText) findViewById(R.id.num1);
+        EditText num2=(EditText) findViewById(R.id.num2);
 
+        EditText result=(EditText) findViewById(R.id.result);
 
+        n1=Integer.parseInt(num1.getText().toString());
+        n2=Integer.parseInt(num2.getText().toString());
+        sub = n1 - n2;
+        result.setText(Integer.toString(sub));
 
+    }
 
+    public void mulnum(View view)
+    {
+        int n1,n2,mul;
+        EditText num1=(EditText) findViewById(R.id.num1);
+        EditText num2=(EditText) findViewById(R.id.num2);
+
+        EditText result=(EditText) findViewById(R.id.result);
+
+        n1=Integer.parseInt(num1.getText().toString());
+        n2=Integer.parseInt(num2.getText().toString());
+        mul = n1 * n2;
+        result.setText(Integer.toString(mul));
+
+    }
+
+    public void divnum(View view)
+    {
+        int n1,n2,div;
+        EditText num1=(EditText) findViewById(R.id.num1);
+        EditText num2=(EditText) findViewById(R.id.num2);
+
+        EditText result=(EditText) findViewById(R.id.result);
+
+        n1=Integer.parseInt(num1.getText().toString());
+        n2=Integer.parseInt(num2.getText().toString());
+        div = n1 / n2;
+        result.setText(Integer.toString(div));
 
     }
 }
